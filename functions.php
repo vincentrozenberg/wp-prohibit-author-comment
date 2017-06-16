b3a30fe9e37b1f962d7c41159f4cd4c34792889d
vincentrozenberg <vincentrozenberg@gmail.com>
2017-06-16 02:23:16 +0200

    Create functions.php

---
+++ functions.php
@@ -0,0 +1,18 @@
+<?php
+
+function prohibit_author_comment( $approved, $commentdata ) {
+
+  $post_id = $commentdata['comment_post_ID']; // get the post ID.
+  $author_id = get_post_field( 'post_author', $post_id ); // Get the author ID. 
+
+	if( $commentdata['user_ID'] == $author_id ) {
+		return 'trash'; // skip everything, comment goes straight to trash.
+
+	}
+
+	return $approved; // This is the approval status set on WP Dashboard.
+}
+
+add_filter( 'pre_comment_approved' , 'prohibit_author_comment' , '99', 2 );
+
+?>
